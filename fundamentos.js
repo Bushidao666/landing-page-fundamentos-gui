5ede700707f020715ebc09a750d278144d12a4e2
Bushidao666 <jprb.siqueira22@hotmail.com>
2025-05-27 21:35:08 -0300

    Agora vai

--- fundamentos.js
+++ fundamentos.js
@@ -258,16 +258,16 @@ document.addEventListener('DOMContentLoaded', function() {
       document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/;SameSite=Lax`;
     }
 
-    // function generateUUID() {
-    //   if (typeof crypto !== 'undefined' && crypto.randomUUID) {
-    //     return crypto.randomUUID();
-    //   }
-    //   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    //     const r = Math.random() * 16 | 0;
-    //     const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    //     return v.toString(16);
-    //   });
-    // }
+    function generateUUID() {
+      if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+        return crypto.randomUUID();
+      }
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    }
 
     /*
     async function sha256(message) {
